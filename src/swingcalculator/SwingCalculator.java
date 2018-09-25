afd565c8fa7eda1969b1d32f55e89f84f68cbae5
Agozie <fagozie43@gmail.com>
2018-09-25 16:20:52 +0100

    Updated SwingClass.java

---
+++ src/swingcalculator/SwingCalculator.java
@@ -0,0 +1,15 @@
+package swingcalculator;
+
+/**
+ *
+ * @author agozie
+ */
+public class SwingCalculator {
+    
+    public static void main(String[] args) {
+        SwingArithmeticJFrame SwingFrame  = new SwingArithmeticJFrame();
+        SwingFrame.setSize(376, 352);
+        SwingFrame.setVisible(true);
+    }
+
+}
